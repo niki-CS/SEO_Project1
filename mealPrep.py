09d7db1c249facd901bcc613dfb57f78cdb4146f
Nicole Gil <nicole.gil228@gmail.com>
2025-06-27 19:54:48 +0000

    comments of general layout

---
+++ mealPrep.py
@@ -0,0 +1,18 @@
+# Meal Prep Shopping List Automator
+# Computer will prompt user for meal that they desire to meal prep for breakfast, lunch, and dinner
+# Shopping list will be generated based on meals specified
+# AI will generate general list based on web results of meal names
+
+# Introduction
+
+
+#Prompt user for meals
+
+
+# API integration
+
+
+# Results
+
+
+# End of program? Ask user if satisfied with results?
